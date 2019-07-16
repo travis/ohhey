b84c8b60fc945bbe9ec713cebd5615d30cc5d186
Travis Vachon <travis.vachon@gmail.com>
2019-07-16 12:31:36 -0700

    add ids to usermeta
    they're required

--- ui/src/stories/data.js
+++ ui/src/stories/data.js
@@ -20,7 +20,7 @@ export const toby = {
 
 export const claim = {
   id: 1, body: "This is a claim", slug: "this-is-a-claim", score: 100, creator: travis,
-  userMeta: {agreement: 100, user: travis}
+  userMeta: {id: "1:travis", agreement: 100, user: travis}
 }
 export const longClaim = {id: 2, body: "This is a claim that stretches the limits of this system. It has precisely 255 characters, the maximum number of characters that a claim can have and still be valid. Anything longer than this will be rejected by the system because it is too long. Goodbye.", slug: "this-is-another-claim",
                           score: 8000, creator: chuchu}
@@ -28,16 +28,17 @@ export const longClaim = {id: 2, body: "This is a claim that stretches the limit
 export const supportingClaim = {
   id: 3, body: "It exists in ohhey", slug: "it-exists-in-ohhey", score: 0,
   creator: travis,
-  userMeta: {agreement: 100, user: toby}
+  userMeta: {id: "3:toby", agreement: 100, user: toby}
 }
 export const supportingEvidence = {id: 100, claim: supportingClaim, supports: true}
 
 export const opposingClaim = {
   id: 4, body: "'This is a claim' is a tautology", slug: "this-is-a-claim-is-a-tautology", score: 100,
   creator: james,
-  userMeta: {agreement: -100, user: ian}
+  userMeta: {id: "4:ian", agreement: -100, user: ian}
 }
-export const opposingEvidence = {id: 101, claim: opposingClaim, supports: false, userMeta: {relevance: 45}}
+export const opposingEvidence = {id: 101, claim: opposingClaim, supports: false,
+                                 userMeta: {id: "101:ian", relevance: 45, user: ian}}
 
 export const claims = [
   claim,
